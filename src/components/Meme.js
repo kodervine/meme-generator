577932950e1cbdaf6dd900dc229739dfa10ea0a9
Laskellyb <anikwenzekelly@gmail.com>
2022-08-17 08:26:43 +0100

    Images should be in the same components folder

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,19 +1,16 @@
 import React from "react"
 import memesData from "../memesData.js"
+import "./style.css"
 
 
 export default function Meme(){
-        /**
-     * Challenge: Get a random image from the `memesData` array
-     * when the "new meme image" button is clicked.
-     * 
-     * Log the URL of the image to the console. (Don't worry
-     * about displaying the image yet)
-     */
+    const [ memeImage, setMemeImage ] = React.useState("")
+        
     function memeDisplay(){
     const memeArray= memesData.data.memes
-    const randomNumber = Math.floor(Math.random() * memeArray.length) 
-    console.log(memeArray[randomNumber].url)
+    const randomNumber = Math.floor(Math.random() * memeArray.length)
+    
+    setMemeImage(memeArray[randomNumber].url)
 }
     
     return(
@@ -22,7 +19,12 @@ export default function Meme(){
                 <input type="text" value="shut up" />
                 <input type="text" value="and take my money" />
             </form>
-            <button className="form-btn" onClick={memeDisplay}>Get a new Meme Image 🖼</button>
+            <div className="form-btn-container">
+                <button className="form-btn" onClick={memeDisplay}>Get a new Meme Image 🖼</button>
+            </div>
+            <div className="meme-gen-img">
+                <img src={memeImage} />
+            </div>       
         </div>
     )
 }
